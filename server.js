29d830ab10c17c9bc9ae649104b1ac50c144ecf1
jhpark322 <jhpark32255@gmail.com>
2026-05-08 23:01:30 +0900

    Add local static server

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const root = __dirname;
+const types = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8"
+};
+
+const server = http.createServer((req, res) => {
+  let requestPath = decodeURIComponent(req.url.split("?")[0]);
+  if (requestPath === "/") requestPath = "/index.html";
+
+  const filePath = path.normalize(path.join(root, requestPath));
+  if (!filePath.startsWith(root)) {
+    res.writeHead(403);
+    res.end("forbidden");
+    return;
+  }
+
+  fs.readFile(filePath, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      res.end("not found");
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Type": types[path.extname(filePath)] || "application/octet-stream"
+    });
+    res.end(data);
+  });
+});
+
+server.listen(4173, "127.0.0.1", () => {
+  console.log("Airdrop Gallery running at http://127.0.0.1:4173");
+});
